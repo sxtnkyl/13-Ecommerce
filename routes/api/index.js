eedb6440b9b4fd7f9405213f3f0b3abe75b31ac4
kyle sexton <ksexton.exe@gmail.com>
2021-07-27 17:41:14 -0400

    tested in postman, all routes working

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,10 @@
-const router = require('express').Router();
-const categoryRoutes = require('./category-routes');
-const productRoutes = require('./product-routes');
-const tagRoutes = require('./tag-routes');
+const router = require("express").Router();
+const categoryRoutes = require("./category-routes");
+const productRoutes = require("./product-routes");
+const tagRoutes = require("./tag-routes");
 
-router.use('/categories', categoryRoutes);
-router.use('/products', productRoutes);
-router.use('/tags', tagRoutes);
+router.use("/category-routes", categoryRoutes);
+router.use("/product-routes", productRoutes);
+router.use("/tag-routes", tagRoutes);
 
 module.exports = router;
